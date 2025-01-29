0ce2a07f35e2ea0dbabe397542274b3f23fae178
ajaycloco <mahato.ajay@cloco.net>
2025-01-29 15:03:13 +0545

    email provider service completed

--- service/emailProvider/emailProvider.go
+++ service/emailProvider/emailProvider.go
@@ -7,46 +7,26 @@ import (
 	"strconv"
 	"bytes"
 	"html/template"
-
 )
 
 
 func SendEmail(subject string, to []string, filePath string, data map[string]interface{}) error {
 	godotenv.Load()
-
-	templateBytes, err := os.ReadFile(filePath)
-	if err != nil {
-		return err
-	}
-	templateContent := string(templateBytes) // Convert []byte to string
-
-	// Parse the HTML template
-	tmpl, err := template.New("email").Parse(templateContent)
-	if err != nil {
-		return err
-	}
-
-	// Execute template with provided data
-	var bodyBuffer bytes.Buffer
-	if err := tmpl.Execute(&bodyBuffer, data); err != nil {
-		return err
-	}
-
-
-
 	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
 	if err != nil {
 		return err
 	}
 
+	body,err := ExecuteHtemplate(filePath, data)
+	if err != nil {
+		return err
+	}
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", "alex@example.com")
 	m.SetHeader("To", to...)
-	m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	m.SetHeader("Subject", subject)
-	m.SetBody("text/html", bodyBuffer.String())
-	// m.Attach("/home/Alex/lolcat.jpg")
+	m.SetBody("text/html", body)
 
 	d := gomail.NewDialer(os.Getenv("SMTP_HOST"), port, os.Getenv("SMTP_USER") , os.Getenv("SMTP_PASS"))
 
@@ -55,4 +35,25 @@ func SendEmail(subject string, to []string, filePath string, data map[string]int
 	}
 	return nil
 
+}
+
+func ExecuteHtemplate(filePath string, data map[string]interface{}) (string, error) {
+	templateBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	templateContent := string(templateBytes)
+
+	tmpl, err := template.New("email").Parse(templateContent)
+	if err != nil {
+		return "", err
+	}
+
+	var bodyBuffer bytes.Buffer
+	if err := tmpl.Execute(&bodyBuffer, data); err != nil {
+		return "", err
+	}
+
+	
+	return bodyBuffer.String(), nil
 }
